b725c9bb1ef10a901002eab0f428fb20e2237d91
root <vineela.tarra@gmail.com>
2022-05-18 08:38:47 +0000

    react 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div className="backGround">
+    <h1 className="heading">SUPER OVER LEAGUE</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="image"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
